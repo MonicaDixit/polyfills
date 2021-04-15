4795f27e52c34fab15d41dbe41c9623fd2637ce3
Mudita <MonicaDixit@users.noreply.github.com>
2021-04-15 11:40:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -367,3 +367,20 @@ function flat(arr, depth = 1) {
 
  return result
 }
+
+/***********************************************************************************************************************
+*Own implementation of extends function
+/***********************************************************************************************************************
+const myExtends = (SuperType, SubType) => {
+    function ExtendedType(...args) {
+      SuperType.call(this, ...args);
+      SubType.call(this, ...args);
+      Object.setPrototypeOf(this, SubType.prototype);
+    }
+  
+    Object.setPrototypeOf(SubType.prototype, SuperType.prototype);
+    Object.setPrototypeOf(ExtendedType.prototype, SubType.prototype);
+    Object.setPrototypeOf(ExtendedType, SuperType);
+  
+   return ExtendedType;
+}
